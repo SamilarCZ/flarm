45b66329f63f79f3e5e020ef156d43c3d04435ea
SamilarCZ <filip.lansky@gmail.com>
2015-04-12 10:32:14 +0200

    test

--- lib/FLaRM/Helper.php
+++ lib/FLaRM/Helper.php
@@ -1,44 +1,54 @@
 <?php
 
-    namespace ActiveRecord;
+namespace ActiveRecord;
 
-    class Helper extends Model{
+class Helper extends Model
+{
 
-        public function addSpaces($str){
-            return ' ' . $str . ' ';
-        }
-        /*
-           $array[0]['cond']['row'] = ' ' . $clause['row'] . ' ';
-           $array[0]['cond']['val'] = ' ' . $clause['val'] . ' ';
-           $array[0]['cond']['operator'] = ' ' . $clause['operator'] . ' ';
-           $array[0]['glue'] = ' ' . $clause['glue'] . ' ';
-        */
-        public function where($where){
-            $conn = ConnectionManager::get_connection("development");
-            $builder = new SQLBuilder($conn, $this->table_name());
-            $utils = new Utils();
-            if(count($where) > 0) {
-                foreach($where as $clause) {
-                    foreach($clause as $key => $value) {
-                        $builder->where($clause);
-                        $utils->add_condition($clause);
-                    }
+    public function addSpaces($str)
+    {
+        return ' ' . $str . ' ';
+    }
+
+    /*
+       $array[0]['cond']['row'] = ' ' . $clause['row'] . ' ';
+       $array[0]['cond']['val'] = ' ' . $clause['val'] . ' ';
+       $array[0]['cond']['operator'] = ' ' . $clause['operator'] . ' ';
+       $array[0]['glue'] = ' ' . $clause['glue'] . ' ';
+    */
+    public function where($where)
+    {
+        $conn = ConnectionManager::get_connection("development");
+        $builder = new SQLBuilder($conn, $this->table_name());
+        $utils = new Utils();
+        if (count($where) > 0) {
+            foreach ($where as $clause) {
+                foreach ($clause as $key => $value) {
+                    $builder->where($clause);
+                    $utils->add_condition($clause);
                 }
             }
         }
+    }
 
-        public function whereAnd(){
-
-        }
-
-        public function order(){
-
-        }
-        public function limit(){
-
-        }
-        public function groupBy(){
-
-        }
+    public function whereAnd()
+    {
 
     }
+
+    public function order()
+    {
+
+    }
+
+    public function limit()
+    {
+
+    }
+
+    public function groupBy()
+    {
+
+    }
+
+}
