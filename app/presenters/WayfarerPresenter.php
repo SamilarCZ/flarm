ee307c643e972019f5bb627e14ea0f2aa2c98371
filip.lansky <filip.lansky@gmail.com>
2015-08-06 15:50:55 +0200

    partial commit - working ....
    new :
    
    drawings !

--- app/presenters/WayfarerPresenter.php
+++ app/presenters/WayfarerPresenter.php
@@ -68,55 +68,56 @@ CREATE TABLE `blocks` (
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 			 * */
 
-//			$this->debug = true;
-			$this->debug_lite = false;
+			$this->debug = true;
+//			$this->debug_lite = false;
 //			ini_set('memory_limit', '1024M');
 			set_time_limit(0);
 			if($this->debug === true || $this->debug_lite === true || $this->debug_time_show === true) $this->debug_time_start = microtime(true);
-			$desiredArray = [12655];
+			$desiredArray = [];
 //			/** minimalni hodnota ! (pokud se jedna o hranici generator vygeneruje vzdy tak, aby byli pozadovane ID existujici) **/
 //			$desiredLastBlockId = 10000;
 //			for($i=0;$i<=($desiredLastBlockId/10000);$i++){
 //				$desiredArray[] = $i*10000;
 //			}
-			foreach($desiredArray as $iteration){
-				$blockArray = $this->generateBlockArray($iteration);
-				foreach($blockArray as $row => $value){
-					foreach($value as $col => $value2){
-						$newBlock['id'] = $value2['block'];
-						if(isset($value2['links']['link_1']['block'])) $newBlock['link_1'] = $value2['links']['link_1']['block'];
-						if(isset($value2['links']['link_2']['block'])) $newBlock['link_2'] = $value2['links']['link_2']['block'];
-						if(isset($value2['links']['link_3']['block'])) $newBlock['link_3'] = $value2['links']['link_3']['block'];
-						if(isset($value2['links']['link_4']['block'])) $newBlock['link_4'] = $value2['links']['link_4']['block'];
-						if(isset($value2['links']['link_5']['block'])) $newBlock['link_5'] = $value2['links']['link_5']['block'];
-						if(isset($value2['links']['link_6']['block'])) $newBlock['link_6'] = $value2['links']['link_6']['block'];
-						if(isset($value2['links']['link_7']['block'])) $newBlock['link_7'] = $value2['links']['link_7']['block'];
-						if(isset($value2['links']['link_8']['block'])) $newBlock['link_8'] = $value2['links']['link_8']['block'];
-						$inserted = $this->blocksModel->insert($newBlock);
-						echo 'inserted block ID: ' . $inserted->getPrimary() . '<br />';
-						if($inserted->getPrimary() % 100 === 0){
-							echo 'must sleep now for few seconds ...<br />';
-							sleep(1);
+			$this->template->draw = $this->drawStoredBlock();
+			if(count($desiredArray) > 0) {
+				foreach ($desiredArray as $iteration) {
+					$blockArray = $this->generateBlockArray($iteration, 10, 25);
+					foreach ($blockArray as $row => $value) {
+						foreach ($value as $col => $value2) {
+							$newBlock['id'] = $value2['block'];
+							if (isset($value2['links']['link_1']['block'])) $newBlock['link_1'] = $value2['links']['link_1']['block'];
+							if (isset($value2['links']['link_2']['block'])) $newBlock['link_2'] = $value2['links']['link_2']['block'];
+							if (isset($value2['links']['link_3']['block'])) $newBlock['link_3'] = $value2['links']['link_3']['block'];
+							if (isset($value2['links']['link_4']['block'])) $newBlock['link_4'] = $value2['links']['link_4']['block'];
+							if (isset($value2['links']['link_5']['block'])) $newBlock['link_5'] = $value2['links']['link_5']['block'];
+							if (isset($value2['links']['link_6']['block'])) $newBlock['link_6'] = $value2['links']['link_6']['block'];
+							if (isset($value2['links']['link_7']['block'])) $newBlock['link_7'] = $value2['links']['link_7']['block'];
+							if (isset($value2['links']['link_8']['block'])) $newBlock['link_8'] = $value2['links']['link_8']['block'];
+							$inserted = $this->blocksModel->insert($newBlock);
+							echo 'inserted block ID: ' . $inserted->getPrimary() . '<br />';
+							if ($inserted->getPrimary() % 100 === 0) {
+								echo 'must sleep now for few seconds ...<br />';
+								sleep(1);
+							}
+							/** DEBUG **/
+							if ($this->debug || $this->debug_lite === true) {
+								$memory_usage_mb = round((memory_get_usage() / 1000000), 2);
+								echo 'Inserting into database consumes ' . $memory_usage_mb . ' MB usage<br />';
+							}
+							/** DEBUG **/
+							ob_flush();
+							flush();
 						}
-						/** DEBUG **/
-						if($this->debug || $this->debug_lite === true) {
-							$memory_usage_mb = round((memory_get_usage() / 1000000), 2);
-							echo 'Inserting into database consumes ' . $memory_usage_mb . ' MB usage<br />';
-						}
-						/** DEBUG **/
-						ob_flush();
-						flush();
 					}
+					if ($this->debug === true || $this->debug_lite === true || $this->debug_time_show === true) $this->debug_time_end = microtime(true);
+					if ($this->debug === true || $this->debug_lite === true || $this->debug_time_show === true) echo 'Generation of world takes : ' . ($this->debug_time_end - $this->debug_time_start) . ' sec<br />';
 				}
-				if($this->debug === true || $this->debug_lite === true || $this->debug_time_show === true) $this->debug_time_end = microtime(true);
-				if($this->debug === true || $this->debug_lite === true || $this->debug_time_show === true) echo 'Generation of world takes : ' . ($this->debug_time_end - $this->debug_time_start) . ' sec<br />';
 			}
 		}
 
-		private function generateBlockArray($block_id = 1){
+		private function generateBlockArray($block_id = 1, $rows = 100, $cols = 50){
 			$rowStart = 1;
-			$rows = 100;
-			$cols = 100;
 			$blocksArray = [];
 			$blockIterator = $block_id;
 			if($block_id !== 1){
@@ -127,7 +128,7 @@ CREATE TABLE `blocks` (
 
 			/** DEBUG **/
 			if($this->debug || $this->debug_lite === true){
-				echo 'GENERATOR SETTING : R[' . $rows . '], C[' . $cols . '], RS[' . $rowStart . ']<br />';
+				echo '<h1>GENERATOR SETTING : R[' . $rows . '], C[' . $cols . '], RS[' . $rowStart . ']</h1>';
 			}
 			/** DEBUG **/
 
@@ -136,31 +137,37 @@ CREATE TABLE `blocks` (
 					$blocksArray[$i][$ii]['block'] = $blockIterator;
 					$blockIterator++;
 				}
-
-				/** DEBUG **/
-				if($this->debug || $this->debug_lite === true) {
-					$memory_usage_mb = round((memory_get_usage() / 1000000), 2);
-					echo 'Createing of blocks consumes ' . $memory_usage_mb . ' MB usage<br />';
-				}
-				/** DEBUG **/
 			}
+			/*
+			echo 'R*C=' . ($rows * $cols) . '<br/>';
+			echo 'R/C=' . ($rows / $cols) . '<br/>';
+			echo 'C/R=' . ($cols / $rows) . '<br/>';
+			echo $this->drawBlockArray($blocksArray);
+			die;
+			*/
+			/** DEBUG **/
+			if($this->debug || $this->debug_lite === true) {
+				$memory_usage_mb = round((memory_get_usage() / 1000000), 2);
+				echo '<h3>Createing of blocks consumes ' . $memory_usage_mb . ' MB usage</h3>';
+			}
+			/** DEBUG **/
 			$blockIterator = $block_id;
 			if($block_id !== 1) $blockIterator = $block_id+1;
 			for($i=$rowStart;$i<=$rows;$i++){
 				for($ii=1;$ii<=$cols;$ii++) {
 					$links = [];
-					if (isset($blocksArray[$i - 1][$ii - 1]['block'])) $links['link_1']['block'] = $blocksArray[$i - 1][$ii - 1]['block'];
-					if (isset($blocksArray[$i - 1][$ii]['block'])) $links['link_2']['block'] = $blocksArray[$i - 1][$ii]['block'];
-					if (isset($blocksArray[$i - 1][$ii + 1]['block'])) $links['link_3']['block'] = $blocksArray[$i - 1][$ii + 1]['block'];
-					if (isset($blocksArray[$i][$ii - 1]['block'])) $links['link_4']['block'] = $blocksArray[$i][$ii - 1]['block'];
-					if (isset($blocksArray[$i][$ii + 1]['block'])) $links['link_5']['block'] = $blocksArray[$i][$ii + 1]['block'];
-					if (isset($blocksArray[$i + 1][$ii - 1]['block'])) $links['link_6']['block'] = $blocksArray[$i + 1][$ii - 1]['block'];
-					if (isset($blocksArray[$i + 1][$ii]['block'])) $links['link_7']['block'] = $blocksArray[$i + 1][$ii]['block'];
-					if (isset($blocksArray[$i + 1][$ii + 1]['block'])) $links['link_8']['block'] = $blocksArray[$i + 1][$ii + 1]['block'];
+					if (isset($blocksArray[$i - 1][$ii - 1]['block'])) 	$links['link_1']['block'] = $blocksArray[$i - 1][$ii - 1]['block'];
+					if (isset($blocksArray[$i - 1][  $ii  ]['block'])) 	$links['link_2']['block'] = $blocksArray[$i - 1][  $ii  ]['block'];
+					if (isset($blocksArray[$i - 1][$ii + 1]['block'])) 	$links['link_3']['block'] = $blocksArray[$i - 1][$ii + 1]['block'];
+					if (isset($blocksArray[  $i  ][$ii - 1]['block'])) 	$links['link_4']['block'] = $blocksArray[  $i  ][$ii - 1]['block'];
+					if (isset($blocksArray[$i    ][$ii + 1]['block'])) 	$links['link_5']['block'] = $blocksArray[  $i  ][$ii + 1]['block'];
+					if (isset($blocksArray[$i + 1][$ii - 1]['block'])) 	$links['link_6']['block'] = $blocksArray[$i + 1][$ii - 1]['block'];
+					if (isset($blocksArray[$i + 1][  $ii  ]['block'])) 	$links['link_7']['block'] = $blocksArray[$i + 1][  $ii  ]['block'];
+					if (isset($blocksArray[$i + 1][$ii + 1]['block'])) 	$links['link_8']['block'] = $blocksArray[$i + 1][$ii + 1]['block'];
 
 					/** DEBUG **/
 					if($this->debug) {
-						echo '<b>got block ' . ($blockIterator) . '(row: ' . $i . ',col: ' . $ii . ') try to search existing links ...</b><br />';
+						echo '<h2>got block ' . ($blockIterator) . '(row: ' . $i . ',col: ' . $ii . ') try to search existing links ...</h2>';
 						echo 'link_1 -> row : ' . ($i - 1) . ', col: ' . ($ii - 1) . ', link_block_id : ' . ($blockIterator - 101) . ' verification = ' . ((isset($blocksArray[$i - 1][$ii - 1])) ? 'TRUE' : 'FALSE') . '<br />';
 						echo 'link_2 -> row : ' . ($i - 1) . ', col: ' . ($ii) . ', link_block_id : ' . ($blockIterator - 100) . ' verification = ' . ((isset($blocksArray[$i - 1][$ii])) ? 'TRUE' : 'FALSE') . '<br />';
 						echo 'link_3 -> row : ' . ($i - 1) . ', col: ' . ($ii + 1) . ', link_block_id : ' . ($blockIterator - 99) . ' verification = ' . ((isset($blocksArray[$i - 1][$ii + 1])) ? 'TRUE' : 'FALSE') . '<br />';
@@ -185,6 +192,60 @@ CREATE TABLE `blocks` (
 
 			}
 			return $blocksArray;
+		}
+
+		public function drawBlockArray(array $blockArray){
+			$paint = '<table>';
+			$i=1;
+			foreach($blockArray as $key => $val){
+				$paint .= '<tr>';
+				foreach($val as $key2 => $val2){
+					$paint .= '<td style="border: 1px solid black; text-align: center; "><small>';
+					$paint .= '(' . $i . ') ' . $key . '/' . $key2 ;
+					$paint .= '</small></td>';
+					$i++;
+				}
+				$paint .= '</tr>';
+			}
+			$paint .= '</table>';
+			return $paint;
+		}
+
+		public function drawStoredBlock(){
+			$blockModel = $this->blocksModel->getAll();
+			$paint = '<table>' . PHP_EOL;
+			foreach($blockModel as $key => $val){
+				if($val['id'] === 1) $paint .= '<tr>' . PHP_EOL;
+					$paint .= '<td style="border: 1px solid black; text-align: center; ">' .  PHP_EOL;
+						$paint .= '<small>' .  PHP_EOL;
+						$paint .= '[' . $val['id'] . ']' .  PHP_EOL;
+						$paint .= '</small>' .  PHP_EOL;
+					$paint .= '</td>' .  PHP_EOL;
+					$paint .= '<td style="border: 1px solid black; text-align: center; ">' .  PHP_EOL;
+						$paint .= '<table>' .  PHP_EOL;
+							$paint .= '<tr style="height: 2em;">' .  PHP_EOL;
+								$paint .= '<td style="width:33%;border: 1px solid black; text-align: center; ' . (($val['link_1'])?'':' background-color:red;') . '">[NE]' . (($val['link_1'])? '( : ' . $val['link_1'] . ')' : '') .'</td>' .  PHP_EOL;
+								$paint .= '<td style="width:33%;border: 1px solid black; text-align: center; ' . (($val['link_2'])?'':' background-color:red;') . '">[N]' . (($val['link_2'])? '( : ' . $val['link_2'] . ')' : '') .'</td>' .  PHP_EOL;
+								$paint .= '<td style="width:33%;border: 1px solid black; text-align: center; ' . (($val['link_3'])?'':' background-color:red;') . '">[NW]' . (($val['link_3'])? '( : ' . $val['link_3'] . ')' : '') .'</td>' .  PHP_EOL;
+							$paint .= '</tr>' .  PHP_EOL;
+							$paint .= '<tr style="height: 2em;">' .  PHP_EOL;
+								$paint .= '<td style="border: 1px solid black; text-align: center; ' . (($val['link_4'])?'':' background-color:red;') . '">[E]' . (($val['link_4'])? '( : ' . $val['link_4'] . ')' : '') .'</td>' .  PHP_EOL;
+								$paint .= '<td style="border: 1px solid black; text-align: center; ">[X]</td>' .  PHP_EOL;
+								$paint .= '<td style="border: 1px solid black; text-align: center; ' . (($val['link_5'])?'':' background-color:red;') . '">[W]' . (($val['link_5'])? '( : ' . $val['link_5'] . ')' : '') .'</td>' .  PHP_EOL;
+							$paint .= '</tr>' .  PHP_EOL;
+							$paint .= '<tr style="height: 2em;">' .  PHP_EOL;
+								$paint .= '<td style="border: 1px solid black; text-align: center; ' . (($val['link_6'])?'':' background-color:red;') . '">[SE]' . (($val['link_6'])? '( : ' . $val['link_6'] . ')' : '') .'</td>' .  PHP_EOL;
+								$paint .= '<td style="border: 1px solid black; text-align: center; ' . (($val['link_7'])?'':' background-color:red;') . '">[S]' . (($val['link_7'])? '( : ' . $val['link_7'] . ')' : '') .'</td>' .  PHP_EOL;
+								$paint .= '<td style="border: 1px solid black; text-align: center; ' . (($val['link_8'])?'':' background-color:red;') . '">[SW]' . (($val['link_8'])? '( : ' . $val['link_8'] . ')' : '') .'</td>' .  PHP_EOL;
+							$paint .= '</tr>' .  PHP_EOL;
+						$paint .= '</table>' .  PHP_EOL;
+					$paint .= '</td>' .  PHP_EOL;
+				if($val['id'] % 4 === 0) $paint .= '</tr><tr>' .  PHP_EOL;
+				if($val['id'] === count($blockModel)) $paint .= '</tr>' . PHP_EOL;
+			}
+			$paint .= '</table>' .  PHP_EOL;
+
+			echo $paint;
 		}
 
 	}
